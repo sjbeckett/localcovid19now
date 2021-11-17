3a63a3c3c138ab0929aec541f16035da3e4c0935
FreyjaBrandelTanis <30274062+freyja-bt@users.noreply.github.com>
2021-11-16 20:48:27 -0500

    finish merge from my repository

--- librariesMinimal.R
+++ librariesMinimal.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(sf)
 library(leaflet)
 library(dplyr)
@@ -21,28 +20,3 @@ library(data.table) #for the fread function (large speed up vs. read.csv for lar
 #for Philippines
 library(googledrive)
 library(pdftools)
-library(readr)
-=======
-
-library(sf)
-library(leaflet)
-library(dplyr)
-library(tidyr)
-library(jsonlite)
-library(lubridate)
-library(httr)
-library(rvest)
-library(rmapshaper) #for the ms_simplify function - simplifies polygons.
-library(stringr)
-
-library(shiny)
-library(RColorBrewer)
-#library(xts)
-library(rgdal)
-
-library(COVID19) #for Chile, Peru,..
-library(data.table) #for the fread function (large speed up vs. read.csv for large files). Just using this for the Germany dataset right now, but could be useful for other files also.
-library(googledrive)
-library(pdftools)
-library(vroom)
->>>>>>> cdc_philippines/main
