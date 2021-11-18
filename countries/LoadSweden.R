7adf8dffb53777af8268194509e0b17c292f3e7a
= <github@atc.io>
2021-11-18 18:54:12 -0500

    Correct file encoding, fix UTF8 chars

--- countries/LoadSweden.R
+++ countries/LoadSweden.R
@@ -1,5 +1,5 @@
 LoadSweden <- function() {
-#Swedish COVID-19 National Statistics from Folkhälsomyndigheten: https://experience.arcgis.com/experience/09f821667ce64bf7be6f9f87457ed9aa/page/page_0/
+#Swedish COVID-19 National Statistics from Folkh?lsomyndigheten: https://experience.arcgis.com/experience/09f821667ce64bf7be6f9f87457ed9aa/page/page_0/
 
   temp <- tempfile()
   download.file(url = "https://fohm.maps.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data", destfile = temp, mode="wb")
@@ -8,25 +8,25 @@ LoadSweden <- function() {
   names(swedenResource)[1] <- 'date'
   swedenResource$date <- as.Date(swedenResource$date)
   SwedenCounty <- names(swedenResource)[3:length(names(swedenResource))]
-  SwedenCounty[SwedenCounty == "Jämtland_Härjedalen"] <- "Jämtland"
-  SwedenCounty[SwedenCounty == "Sörmland"] <- "Södermanland"
-  SwedenCounty[SwedenCounty == "Västra_Götaland"] <- "Västra Götaland"
+  SwedenCounty[SwedenCounty == "JÃ¤mtland_HÃ¤rjedalen"] <- "JÃ¤mtland"
+  SwedenCounty[SwedenCounty == "SÃ¶rmland"] <- "SÃ¶dermanland"
+  SwedenCounty[SwedenCounty == "VÃ¤stra_GÃ¶taland"] <- "VÃ¤stra GÃ¶taland"
   names(swedenResource) = c(names(swedenResource)[1:2],SwedenCounty)
-  
-  data = swedenResource %>% 
+
+  data = swedenResource %>%
     pivot_longer(3:23, names_to="County", values_to="cases") %>%
     select(-Totalt_antal_fall) %>%
     arrange(desc(date))
-  
+
   #geometry
   #geom <<- st_read("https://raw.githubusercontent.com/appliedbinf/covid19-event-risk-planner/master/COVID19-Event-Risk-Planner/map_data/sweden-counties.geojson")
   geom<-st_read("countries/data/geom/geomSweden.geojson")
-  
+
   #population
   #pop <- read.csv("https://raw.githubusercontent.com/appliedbinf/covid19-event-risk-planner/master/COVID19-Event-Risk-Planner/map_data/sweden_pop.csv", encoding = 'UTF-8')
   pop <-read.csv("countries/data/Sweden_pop.csv", encoding="UTF-8")
 
-  
+
   data_cur <<- data %>%
     group_by(County) %>%
     summarise(County = first(County), cases = sum(cases), date = first(date)) %>%
@@ -39,16 +39,16 @@ LoadSweden <- function() {
     as.data.frame()
   data_join <<- data_cur %>%
     inner_join(data_past, by = "County", suffix = c("", "_past")) %>%
-    inner_join(pop, by = c("County")) 
+    inner_join(pop, by = c("County"))
   data_join$Difference <- (data_join$cases - data_join$cases_past)*10/14
-  
-#integrate datasets  
+
+#integrate datasets
   SwedenMap <- inner_join(geom,data_join, by = c("name"='County'))
   SwedenMap$RegionName = paste0(SwedenMap$name,", Sweden")
   SwedenMap$Country = "Sweden"
-  SwedenMap$DateReport = as.character(SwedenMap$date) 
+  SwedenMap$DateReport = as.character(SwedenMap$date)
   SwedenMap$pInf = SwedenMap$Difference/SwedenMap$Population
   SWEDEN_DATA = subset(SwedenMap,select=c("DateReport","RegionName","Country","pInf","geometry"))
-  
+
   return(SWEDEN_DATA)
 }
