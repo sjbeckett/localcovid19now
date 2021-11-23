83888917ce42dfdc2ad8d1073c5b1cffa0037666
Stephen Beckett <beckett.stephenj@gmail.com>
2021-11-22 19:31:06 -0500

    update to reflect calls to new files

--- countries/LoadCanada.R
+++ countries/LoadCanada.R
@@ -2,18 +2,14 @@ LoadCanada <- function(){
 # Data aggregated by the COVID-19 Canada Open Data Working Group https://github.com/ccodwg/Covid19Canada
 # Berry I, Soucy J-PR, Tuite A, Fisman D. Open access epidemiologic data and an interactive dashboard to monitor the COVID-19 outbreak in Canada. CMAJ. 2020 Apr 14;192(15):E420. doi:  https://doi.org/10.1503/cmaj.75262.
 
-
 CANADADATA<- vroom::vroom("https://github.com/ishaberry/Covid19Canada/raw/master/timeseries_hr/cases_timeseries_hr.csv")
 
-#geomCAN= st_read("OpenContent_CoVID19Boundary_2020/OpenContent_COVID19Boundary_2020/data/covidHR.shp")
-#geomCAN = st_transform(geomCAN, "+proj=longlat +datum=WGS84")
-geomCAN = st_read("countries/data/geom/CanadaRegionalHealthBoundaries.geojson")
-
-CAN_LINK<- read.csv("https://raw.githubusercontent.com/ishaberry/Covid19Canada/master/other/hr_map.csv",encoding="UTF-8")#contains pop and health region ID codes
+#CAN_LINK<- read.csv("https://raw.githubusercontent.com/ishaberry/Covid19Canada/master/other/hr_map.csv",encoding="UTF-8")#contains pop and health region ID codes
+CAN_LINK <- read.csv("countries/data/Canada_pop.csv")
 
 #shrink this data file
 DATES = as.Date(CANADADATA$date_report,format="%d-%m-%Y")
-DatIND = which(DATES<"2020-10-01")
+DatIND = which(DATES<"2021-10-01")
 CANDATASMALLER = CANADADATA[-DatIND,]
 
 #Assemble the data required to calculate risk scores and match health region ID's
@@ -47,31 +43,41 @@ for( aa in 1:length(CPro) ){
 }
 
 CANADARISK = as.data.frame(CANADARISK)
+
+#geometry
+
+#geomCAN= st_read("OpenContent_CoVID19Boundary_2020/OpenContent_COVID19Boundary_2020/data/covidHR.shp")
+#geomCAN = st_transform(geomCAN, "+proj=longlat +datum=WGS84")
+#st_write(geomCAN,"countries/data/geom/CanadaRegionalHealthBoundaries.geojson")
+#geomCAN = st_read("countries/data/geom/CanadaRegionalHealthBoundaries.geojson")
 #Edit region codes for SK
-geomCAN$HR_UID[95:96] = c("475","474")
+#geomCAN$HR_UID[95:96] = c("475","474")
 #Merge polygons in SK
 #Merge Central West and East
-HMM<- st_union(geomCAN[c(87,88),])%>% st_cast("MULTIPOLYGON")
-geomCAN$geometry[87] = HMM
-geomCAN$HR_UID[87] = "473"
-geomCAN$ENGNAME[87] = "Central"
+#HMM<- st_union(geomCAN[c(87,88),])%>% st_cast("MULTIPOLYGON")
+#geomCAN$geometry[87] = HMM
+#geomCAN$HR_UID[87] = "473"
+#geomCAN$ENGNAME[87] = "Central"
 #Merge Far North Central, West and East
-HMM<- st_union(geomCAN[c(89,90,91),])%>% st_cast("MULTIPOLYGON")
-geomCAN$geometry[89] = HMM
-geomCAN$HR_UID[89] = "471"
-geomCAN$ENGNAME[89] = "Far North"
+#HMM<- st_union(geomCAN[c(89,90,91),])%>% st_cast("MULTIPOLYGON")
+#geomCAN$geometry[89] = HMM
+#geomCAN$HR_UID[89] = "471"
+#geomCAN$ENGNAME[89] = "Far North"
 #Merge North Central, West and East
-HMM<- st_union(geomCAN[c(92,93,94),])%>% st_cast("MULTIPOLYGON")
-geomCAN$geometry[92] = HMM
-geomCAN$HR_UID[92] = "472"
-geomCAN$ENGNAME[92] = "North"
+#HMM<- st_union(geomCAN[c(92,93,94),])%>% st_cast("MULTIPOLYGON")
+#geomCAN$geometry[92] = HMM
+#geomCAN$HR_UID[92] = "472"
+#geomCAN$ENGNAME[92] = "North"
 #Merge South Central, West and East
-HMM<- st_union(geomCAN[c(97,98,99),])%>% st_cast("MULTIPOLYGON")
-geomCAN$geometry[97] = HMM
-geomCAN$HR_UID[97] = "476"
-geomCAN$ENGNAME[97] = "South"
+#HMM<- st_union(geomCAN[c(97,98,99),])%>% st_cast("MULTIPOLYGON")
+#geomCAN$geometry[97] = HMM
+#geomCAN$HR_UID[97] = "476"
+#geomCAN$ENGNAME[97] = "South"
 #remove uneeded regions
-geomCAN=geomCAN[-c(88,90,91,93,94,98,99),]
+#geomCAN=geomCAN[-c(88,90,91,93,94,98,99),]
+#st_write(geomCAN,"countries/data/geom/geomCanada.geojson")
+
+geomCAN = st_read("countries/data/geom/geomCanada.geojson")
 
 
 #integrate datasets
