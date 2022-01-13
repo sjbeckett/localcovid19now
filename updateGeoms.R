2166113119a543b36d8b1e526a6b249287b956df
FreyjaBrandelTanis <30274062+freyja-bt@users.noreply.github.com>
2022-01-13 18:04:15 -0500

    update ghana and taiwan geometries (+ everything else bc simplification process)

--- updateGeoms.R
+++ updateGeoms.R
@@ -511,6 +511,27 @@ geomGermany%>%
   st_write("countries/data/temp_geom/geomGermany.geojson")
 #rm(geomGermany)
 
+# Ghana
+
+geomGhana <- st_read("countries/data/orig_geom/geomGhana.geojson")
+
+geomGhana$macro_code = c("SV","BO", "AF", "BE", "AA", "OT", "WP", "WN", "UW", "UE", "EP", "CP", "AH", "NE", "NP", "TV")
+
+geomGhana <- geomGhana%>%
+  mutate(country_name = "Ghana")%>%
+  inner_join(m49, by=c("country_name" = "CountryorArea"))%>%
+  select(
+    m49code = M49Code,
+    iso3 = ISOalpha3Code,
+    country_name,
+    macro_code,
+    macro_name = REGION
+  )%>%
+  st_make_valid()%>%
+  mutate(across(.cols=ends_with("code"),.fns=as.character))
+
+st_write(geomGhana, "countries/data/temp_geom/geomGhana.geojson")
+
 # India"
 
 geomIndia <- st_read("countries/data/orig_geom/geomIndia.geojson")
@@ -1076,7 +1097,8 @@ geomTaiwan <- geomTaiwan%>%
     micro_code = COUNTYSN,
     micro_name = COUNTYNAME
   )%>%
-  mutate(across(.cols=ends_with("code"),.fns=as.character))
+  mutate(across(.cols=ends_with("code"),.fns=as.character))%>%
+  filter(!micro_code %in% c("10011001","10006003"))
 
 geomTaiwan%>%
   st_write("countries/data/temp_geom/geomTaiwan.geojson")
