27844769212cd151ee0aa816b856305f7b41f734
FreyjaBrandelTanis <30274062+freyja-bt@users.noreply.github.com>
2022-05-10 11:08:42 -0400

    Test: Integrate UpdateGeometries

---
+++ R/addNewGeom.R
@@ -0,0 +1,91 @@
+#' Move Processed Files
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+moveFiles <- function(x) {
+  file.copy(
+    from = here::here("tools", "toProcess", x),
+    to = here::here("tools", "processed", x)
+  )
+  file.remove(here::here("tools", "toProcess", x))
+}
+
+#' Add New Country Geometries
+#'
+#' @return
+#' @export
+#'
+#' @examples
+addNewGeoms <- function() {
+  # Fields will be as follows:
+  ## geoid = ISO3m49_micro_macro
+  ## m49code
+  ## iso3
+  ## county_name
+  ## macro_code
+  ## macro_name
+  ## micro_code
+  ## micro_name
+  ## geometry
+
+  necessary_cols <- dplyr::tibble("macro_code" = NA_character_, "macro_name" = NA_character_, "micro_code" = NA_character_, "micro_name" = NA_character_)
+
+  ## Load in the m49 codes for countries
+  utils::data("m49", envir = environment())
+
+  file_list <- list.files(here::here("tools", "toProcess"))
+
+  if (length(file_list) > 0) {
+    # This "source" needs to be replaced bc it's bad form in a package, but I haven't figured out how it would work better yet
+    lapply(file_list, \(x) source(here::here("tools", "toProcess", x)))
+    geom_list <- paste0("geom", stringr::str_extract(file_list, "(?<=process)[:alpha:]+(?=.R$)"))
+
+    worldNew <- purrr::map_df(
+      geom_list,
+      ~ get(.x) %>%
+        dplyr::mutate(
+          filename = .x
+        ) %>%
+        sf::st_collection_extract() %>%
+        sf::st_cast("MULTIPOLYGON")
+    ) %>%
+      dplyr::left_join(necessary_cols) %>%
+      tidyr::replace_na(list("macro_code" = "00", "micro_code" = "00")) %>%
+      dplyr::mutate(
+        geoid = paste(paste0(iso3, m49code), macro_code, micro_code, sep = "_")
+      ) %>%
+      dplyr::select(geoid, m49code, iso3, country_name, macro_code, macro_name, dplyr::everything()) %>%
+      sf::st_as_sf() %>%
+      tibble::remove_rownames()
+
+    lapply(file_list, moveFiles)
+
+
+    return(worldNew)
+  }
+}
+
+#' Reset processed files
+#'
+#' @return
+#' @export
+#'
+#' @examples
+resetNewGeoms <- function() {
+  file_list <- list.files(here::here("tools", "processed"))
+
+  lapply(
+    file_list,
+    \(x){
+      file.copy(
+        from = here::here("tools", "processed", x),
+        to = here::here("tools", "toProcess", x)
+      )
+      file.remove(here::here("tools", "processed", x))
+    }
+  )
+}
---
+++ R/remSurplus.R
@@ -0,0 +1,36 @@
+#' Remove Surplus Geometries
+#'
+#' @param input_file 
+#' @param collection_files 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' exampledf <- data.frame("filename"=sample(letters, 10), "iso2"=sample(LETTERS, 10))
+#' remSurplus(exampledf)
+remSurplus <- function(
+  input_file,
+  collection_files = c("geomEurope","geomSmallCountries")
+) {
+  isoFile <- input_file%>%
+    sf::st_drop_geometry()%>%
+    dplyr::select(iso3, filename)%>%
+    dplyr::distinct()
+  
+  coll1Iso <- isoFile%>%dplyr::filter(filename==collection_files[1])%>%dplyr::pull(iso3)
+  distIso <- isoFile%>%dplyr::filter(!filename %in% collection_files)%>%dplyr::pull(iso3)
+  
+  input_file <- input_file%>%
+    dplyr::mutate(
+      isDup = dplyr::case_when(
+        filename == collection_files[2] & iso3 %in% coll1Iso ~ 1,
+        filename %in% collection_files & iso3 %in% c(distIso, "ASM", "GUM", "MNP", "VIR","PRI") ~ 1,
+        TRUE ~ 0
+      )
+    )%>%
+    dplyr::filter(isDup == 0)%>%
+    dplyr::select(-isDup)
+  
+  return(input_file)
+}
